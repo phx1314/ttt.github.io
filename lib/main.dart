6185a7d5ae46ccf3f3c9d0053aca299731a7afd6
daif <daif@deepblueai.com>
2020-01-19 14:13:21 +0800

    21

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
